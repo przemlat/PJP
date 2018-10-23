85dc5c39636e5f1b1b85d6d55ef4b42837c8a601
Przemek Latoch <Przemyslaw.Latoch@vimn.com>
2018-10-23 15:54:31 +0200

    Initial commit

---
+++ src/main/groovy/project3/Exercise_3_4.groovy
@@ -0,0 +1,15 @@
+package project3
+
+def plnToEuroRatio = 4.30
+
+def aText = "kot: 120.00 PLN pies: 150.50 PLN chomik: 10.50 PLN"
+aText = aText.replaceAll(/(\d+\.?,?\d+) PLN/) { all, price ->
+    String.format("%.2f", price.toBigDecimal() / plnToEuroRatio) + ' EUR'
+}
+println "a)\n" + aText
+
+def bText = "kot: 120,00 PLN pies: 150,50 PLN chomik: 10,50 PLN"
+bText = bText.replaceAll(/(\d+\.?,?\d+) PLN/) { all, price ->
+    String.format("%.2f", price.replace(",", ".").toBigDecimal() / plnToEuroRatio) + ' EUR'
+}
+println "b)\n" + bText
